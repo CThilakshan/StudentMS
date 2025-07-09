1914ee4b2fa70c037bffcaddd19e228bb61c99bb
CThilakshan <shan17share@gmail.com>
2025-07-09 11:50:54 +0530

    Add project files.

---
+++ StudentMS.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentMS.Web.Models;
+
+namespace StudentMS.Web.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ StudentMS.Web/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentMS.Web.Models
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
